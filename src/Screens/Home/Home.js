4c90800f9941bc7742fe9475fdec64a581dd21cc
Eryk Olliver <erykolliver@gmail.com>
2025-05-04 15:19:45 -0300

    Projeto iniciado - semi-configurado

---
+++ src/Screens/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { View, Text } from "react-native"
+import HomeStyle from "./style/HomeStyle"
+
+export default function Home() {
+    return (
+        <View>
+
+        </View>
+    )
+}
---
+++ src/Screens/Home/style/HomeStyle.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const HomeStyle = StyleSheet.create({
+    Banner:{
+        
+    }
+})
+
+export default HomeStyle;
