4074de2ad7e1d11c24caba015ccb0f5b49d2406d
Eryk Olliver <erykolliver@gmail.com>
2025-06-10 07:57:52 -0300

    config: background e containers adicionado

--- src/Screens/Profile/Profile.js
+++ src/Screens/Profile/Profile.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { View, Text } from "react-native"
 import ProfileStyle from "./style/ProfileStyle"
+import { LinearGradient } from "expo-linear-gradient"
+import { Primmary_Colors } from "../../components/DesigneTokens/pallets"
 
 export default function Profile() {
     return (
-        <View>
-
+        <View style={ProfileStyle.body}>
+            <LinearGradient colors={[Primmary_Colors.BluishWhite, Primmary_Colors.Azure]} style={ProfileStyle.background} start={{ x: 1, y: 0 }} end={{ x: 0, y: 0 }} />
+            <View style={ProfileStyle.header}></View>
+            <View style={ProfileStyle.MainContent}></View>
         </View>
     )
 }
--- src/Screens/Profile/style/ProfileStyle.js
+++ src/Screens/Profile/style/ProfileStyle.js
@@ -1,7 +1,31 @@
 import { StyleSheet } from "react-native";
+import { Screen_Size } from "../../../components/DesigneTokens/metrics";
+import { Secondary_Colors } from "../../../components/DesigneTokens/pallets";
 
 const ProfileStyle = StyleSheet.create({
-    Banner: {
+    body: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        gap: 8
+    },
+    background: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0,
+        height: Screen_Size.height
+    },
+    header: {
+        backgroundColor: Secondary_Colors.White90,
+        width: Screen_Size.width,
+        height: "28%"
+    },
+    MainContent: {
+        backgroundColor: Secondary_Colors.White90,
+        width: Screen_Size.width,
+        height: "72%"
 
     }
 })
