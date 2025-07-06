c995782fd677019d6609a2421ccda1d79c238ce1
Eryk Olliver <erykolliver@gmail.com>
2025-07-06 17:51:30 -0300

    config: svg e d3 instalados

--- src/Screens/Chart/components/MainContent.js
+++ src/Screens/Chart/components/MainContent.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { View, Text } from "react-native"
 import ChartStyle from "../style/ChartStyle"
-import BarChart from "./Charts/BarChart"
 
 export default function MainContent() {
     return (
         <View style={ChartStyle.MainContent}>
+            {/* <BarChart> */}
+            {/* <BarChart> */}
+            {/* <BarChart> */}
         </View>
     )
 }
---
+++ src/components/ChartsBase/BarChart/BarChart.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Svg, { Rect } from "react-native-safe-area-context";
+
+type BarChartProps = {
+    data: string[]
+}
+
+export default function BarChart(props: BarChartProps) {
+    return (
+        <View>
+        </View>
+    )
+}
