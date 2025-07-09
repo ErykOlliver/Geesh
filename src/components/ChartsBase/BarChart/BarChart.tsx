5271b310544e8f690a6d26070a508b425fc96b27
Eryk Olliver <erykolliver@gmail.com>
2025-07-09 17:15:01 -0300

    feat: pastas principais do BarChart criado

--- src/components/ChartsBase/BarChart/BarChart.tsx
+++ src/components/ChartsBase/BarChart/BarChart.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import { View, Text } from "react-native";
-import Svg, { Rect } from "react-native-safe-area-context";
+import { useState } from "react";
+import { View } from "react-native";
+import Svg, { Rect } from "react-native-svg";
+import style from "./style/style";
+
 
 type BarChartProps = {
-    data: string[]
+    data: string[];
 }
 
+const Aspect_Ration = 9 / 16;
+
+
 export default function BarChart(props: BarChartProps) {
+    const [width, setWidth] = useState(0);
+    const height = width * Aspect_Ration;
+
     return (
-        <View>
+        <View style={style.container} onLayout={({ nativeEvent }) => setWidth(nativeEvent.layout.width)}>
+            <Svg>
+                <Rect></Rect>
+            </Svg>
         </View>
     )
 }
