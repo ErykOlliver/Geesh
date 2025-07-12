44ce1cb9d932fdd5faa58de7b49d58cdf7ff1d5d
Eryk Olliver <erykolliver@gmail.com>
2025-07-12 14:25:02 -0300

    feat: modulo de login inicializado

---
+++ src/Screens/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { View, Text } from "react-native"
+import LoginStyle from "./style/LoginStyle"
+
+export default function Login() {
+    return (
+        <View style={LoginStyle.Body}>
+
+        </View>
+    )
+}
