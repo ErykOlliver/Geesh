e6870afc74c38f5d260d54fc3523049c93f71c61
Eryk Olliver <erykolliver@gmail.com>
2025-07-25 19:13:51 -0300

    feat: header do grafico semi finalizado. grafico de barras 2/3

--- src/components/ChartsBase/BarChart/BarChart.tsx
+++ src/components/ChartsBase/BarChart/BarChart.tsx
@@ -2,6 +2,9 @@ import { Text, View } from "react-native";
 import BarChartBase from "./BarChartBase";
 import { Geesh_Fonts } from "../../DesigneTokens/fonts";
 import { Text_Sizes } from "../../DesigneTokens/metrics";
+import { HugeiconsIcon } from "@hugeicons/react-native";
+import DropdownComponent from "../../Reuse/DropDownBase/DropDown";
+
 
 
 
@@ -11,15 +14,21 @@ type BarChartProps = {
     labels: string[];
 }
 
+const Options = [
+    { label: 'Teste', value: 1 }
+]
 
 
 export default function BarChart(props: BarChartProps) {
     return (
         <View style={{ width: '100%', display: 'flex', flexDirection: 'column', borderWidth: 1 }}>
-            <View style={{ borderWidth: 1, width: '100%', justifyContent: 'space-between' }}>
-                <Text style={{ paddingVertical: 6, fontFamily: Geesh_Fonts.InriaBold, fontSize: Text_Sizes.h3 }}>
+            <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, width: '100%', justifyContent: 'space-between' }}>
+                <Text style={{ paddingVertical: 6, borderWidth: 1, fontFamily: Geesh_Fonts.InriaBold, fontSize: Text_Sizes.h3 }}>
                     {props.title}
                 </Text>
+                <View style={{ borderWidth: 1 }} >
+                    <DropdownComponent op={Options} />
+                </View>
             </View>
             <BarChartBase data={props.data} labels={props.labels} />
         </View>
---
+++ src/components/Reuse/DropDownBase/DropDown.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Primmary_Colors, Secondary_Colors } from '../../DesigneTokens/pallets';
+import { Text_Sizes } from '../../DesigneTokens/metrics';
+import { Geesh_Fonts } from '../../DesigneTokens/fonts';
+
+type options = {
+    op: any[]
+}
+
+type props = {
+
+}
+
+
+
+const DropdownComponent = (options: options) => {
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
+
+    const renderLabel = () => {
+        if (value || isFocus) {
+            return (
+                <Text style={[styles.label, isFocus && { color: 'blue' }]}>
+                </Text>
+            );
+        }
+        return null;
+    };
+
+    return (
+        <View>
+            {renderLabel()}
+            <Dropdown
+                style={[styles.dropdown, isFocus && { borderColor: Primmary_Colors.Azure, borderWidth: 0.5 }]}
+                selectedTextStyle={styles.selectedTextStyle}
+                data={options.op}
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                value={value}
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
+                onChange={item => {
+                    setValue(item.value);
+                    setIsFocus(false);
+                }}
+            />
+        </View>
+    );
+};
+
+export default DropdownComponent;
+
+const styles = StyleSheet.create({
+    dropdown: {
+        height: 40,
+        width: 150,
+        backgroundColor: Secondary_Colors.White70,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+    },
+    label: {
+        position: 'absolute',
+        left: 22,
+        top: 8,
+        zIndex: 999,
+        paddingHorizontal: 8,
+        fontSize: 14,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+        fontFamily: Geesh_Fonts.InriaRegular
+    },
+});
