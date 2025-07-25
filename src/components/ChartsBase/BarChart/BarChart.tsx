47d3fed04bf0f58de66d51b0ff1382e854cfbdf9
Eryk Olliver <erykolliver@gmail.com>
2025-07-25 17:23:20 -0300

    refactor: grafico de barras semi finalizado 1/3

--- src/components/ChartsBase/BarChart/BarChart.tsx
+++ src/components/ChartsBase/BarChart/BarChart.tsx
@@ -15,10 +15,7 @@ type BarChartProps = {
 
 const maxValue = 250;
 const step = 50;
-const spaceBetweenBars = 10;
 const chartHeight = 250;
-
-const barWidth = 20;
 
 
 export default function BarChart(props: BarChartProps) {
@@ -32,26 +29,19 @@ export default function BarChart(props: BarChartProps) {
     for (let v = 0; v <= maxValue; v += step) yTicks.push(v);
 
     return (
-        <View
-            style={{
-                width: '100%',
-                height: chartHeight
-            }}
-            onLayout={(event) => {
-                const { width, height } = event.nativeEvent.layout;
-                if (ChartSize.width !== width || ChartSize.height !== height) {
-                    SetChartSize({ width, height });
-                }
-            }}>
-
+        <View style={{ width: '100%', height: chartHeight, display: 'flex', alignItems: 'flex-end' }} onLayout={(event) => {
+            const { width, height } = event.nativeEvent.layout;
+            if (ChartSize.width !== width || ChartSize.height !== height) {
+                SetChartSize({ width, height });
+            }
+        }}>
             <View style={{ display: 'flex', flexDirection: 'row', width: '100%', height: '100%' }}>
 
-
-                <View style={{ display: 'flex', paddingHorizontal: 5, alignItems: 'center', justifyContent: 'center', height: ChartSize.height }}>
+                <View style={{ display: 'flex', padding: 5, alignItems: 'center', justifyContent: 'center', height: ChartSize.height }}>
                     <ValueInLeft data={yTicks.reverse()} />
                 </View>
 
-                <View style={{ height: ChartSize.height, width: ChartSize.width - 38, overflow: 'hidden' }}>
+                <View style={{ display: 'flex', flexDirection: 'column', height: ChartSize.height, width: ChartSize.width - 38, overflow: 'hidden' }}>
                     <Svg width='100%' height='100%'>
                         {[0, 0.25, 0.5, 0.75, 1].map((ratio, index) => {
                             const y = ChartSize.height * ratio;
@@ -97,29 +87,33 @@ export default function BarChart(props: BarChartProps) {
                             );
                         })}
                     </Svg>
-                    <View style={{ borderWidth: 2, borderColor: 'pink', width: '100%', height: sizeBottomBarOfMonths }}>
-                        <Svg width={ChartSize.width} height={sizeBottomBarOfMonths}>
-                            {props.labels.map((label, index) => {
-                                const x = index * (barWidth + spaceBetweenBars);
-                                const labelY = ChartSize.height + 20;
-                                return (
-                                    <Text
-                                        key={index}
-                                        x={x}
-                                        y={labelY}
-                                        fontSize={sizeOfTextMonths}
-                                        fill="#333"
-                                        textAnchor="middle"
-                                        transform={`rotate(-45, ${x + 22}, ${labelY})`}
-                                        fontFamily={Geesh_Fonts.InriaRegular}
-                                    >
-                                        {label}
-                                    </Text>
-                                );
-                            })}
-                        </Svg>
-                    </View>
                 </View>
+            </View>
+            <View style={{ display: 'flex', width: ChartSize.width - 35, height: sizeBottomBarOfMonths, justifyContent: 'flex-end', alignItems: 'flex-start' }}>
+                <Svg width={ChartSize.width} height={sizeBottomBarOfMonths}>
+                    {props.labels.map((label, index) => {
+                        const totalBars = props.data.length;
+                        const dynamicSpacing = 15;
+                        const dynamicBarWidth = (ChartSize.width - 38 - (totalBars - 1) * dynamicSpacing) / totalBars;
+
+                        const x = index * (dynamicBarWidth + dynamicSpacing);
+                        const labelY = sizeBottomBarOfMonths;
+                        return (
+                            <Text
+                                key={index}
+                                x={x + 28}
+                                y={labelY}
+                                fontSize={sizeOfTextMonths}
+                                fill="#333"
+                                textAnchor="middle"
+                                transform={`rotate(-45, ${x}, ${labelY})`}
+                                fontFamily={Geesh_Fonts.InriaRegular}
+                            >
+                                {label}
+                            </Text>
+                        );
+                    })}
+                </Svg>
             </View>
         </View>
     )
