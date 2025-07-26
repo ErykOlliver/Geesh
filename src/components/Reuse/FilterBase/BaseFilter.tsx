1aad332e4ccb93c252273a3796142320f6add0cc
Eryk Olliver <erykolliver@gmail.com>
2025-07-26 11:56:35 -0300

    feat: type e parametros da função incrementados

--- src/components/Reuse/FilterBase/BaseFilter.tsx
+++ src/components/Reuse/FilterBase/BaseFilter.tsx
@@ -1,10 +1,14 @@
 import { View } from "react-native";
 import BaseFilterStyle from "./style/BaseFilterStyle.js";
 
-export default function BaseFilter() {
+type FilterProps = {
+    title: string
+}
+
+export default function BaseFilter(props: FilterProps) {
     return (
         <View style={BaseFilterStyle.body}>
-
+            
         </View>
     )
 }
