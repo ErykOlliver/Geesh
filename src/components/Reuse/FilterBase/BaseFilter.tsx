9737496861db60d58678b1da55e322cc2cf476b1
Eryk Olliver <erykolliver@gmail.com>
2025-07-26 11:55:05 -0300

    feat: base do filtro montada

---
+++ src/components/Reuse/FilterBase/BaseFilter.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import BaseFilterStyle from "./style/BaseFilterStyle.js";
+
+export default function BaseFilter() {
+    return (
+        <View style={BaseFilterStyle.body}>
+
+        </View>
+    )
+}
---
+++ src/components/Reuse/FilterBase/style/BaseFilterStyle.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const BaseFilterStyle = StyleSheet.create({
+    body: {
+
+    }
+})
+
+export default BaseFilterStyle;
