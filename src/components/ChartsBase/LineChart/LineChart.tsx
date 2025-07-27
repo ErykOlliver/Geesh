1e7720169104775c799350313cbf858ed346ec62
Eryk Olliver <erykolliver@gmail.com>
2025-07-27 18:17:49 -0300

    feat: grafico de linha inicializado

---
+++ src/components/ChartsBase/LineChart/LineChart.tsx
@@ -0,0 +1,36 @@
+import { Text, View } from "react-native";
+import BarChartBase from "./BarChartBase.tsx";
+import { Geesh_Fonts } from "../../DesigneTokens/fonts.js";
+import { Text_Sizes } from "../../DesigneTokens/metrics.js";
+import { HugeiconsIcon } from "@hugeicons/react-native";
+import { FilterVerticalIcon } from "@hugeicons/core-free-icons";
+import BaseFilter from "../../Reuse/FilterBase/BaseFilter.tsx";
+import style from "./style/style.tsx";
+
+
+type BarChartProps = {
+    title?: string;
+    data?: number[];
+    labels: string[];
+}
+
+const Options = [
+    { label: 'Teste', value: 1 }
+]
+
+
+export default function BarChart(props: BarChartProps) {
+    return (
+        <View style={style.body}>
+            <View style={style.header}>
+                <Text style={style.Title}>
+                    {props.title}
+                </Text>
+                <View style={style.FilterPlace} >
+                    <BaseFilter title="Filter" />
+                </View>
+            </View>
+            <BarChartBase data={props.data} labels={props.labels} />
+        </View>
+    )
+}
