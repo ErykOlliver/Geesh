e40bf36c9aad40c8cfe97349827ee48f7c8d2282
Eryk Olliver <erykolliver@gmail.com>
2025-07-27 22:44:13 -0300

    feat: operação ternaria para verificação do tipo da option incrementada

--- src/components/Reuse/Option/OptionComponent.tsx
+++ src/components/Reuse/Option/OptionComponent.tsx
@@ -2,13 +2,24 @@ import { View } from "react-native";
 import { useState } from "react";
 
 type OptionProps = {
+    icon: React.ReactNode;
+    title: string;
+    toggle: boolean;
     function?: () => void;
 }
 
 export default function OptionComponent(props: OptionProps) {
     return (
         <View>
+            {!props.toggle ? (
+                <View>
+                    
+                </View>
+            ) : (
+                <View>
 
+                </View>
+            )}
         </View>
     )
 }
