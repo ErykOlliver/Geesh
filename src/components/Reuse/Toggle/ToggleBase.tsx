29ea5d0ab3469e3170d23c4036dfdf6229aefaf4
Eryk Olliver <erykolliver@gmail.com>
2025-07-27 21:41:34 -0300

    feat: verificação ternaria aplicada para ativar o toggle

--- src/components/Reuse/Toggle/ToggleBase.tsx
+++ src/components/Reuse/Toggle/ToggleBase.tsx
@@ -1,13 +1,17 @@
 import { TouchableOpacity, View } from "react-native";
 import ToggleStyle from "./style/style";
+import { useState } from "react";
 
 type ToggleProps = {
     function?: () => void;
 }
 
 export default function ToggleBase(props: ToggleProps) {
+    const [toggle, setToggle] = useState(false)
+
+    const onToggle = () => setToggle(!toggle);
     return (
-        <TouchableOpacity style={ToggleStyle.toggle}>
+        <TouchableOpacity style={!toggle ? ToggleStyle.onToggle : ToggleStyle.offToggle} onPress={onToggle}>
             <View style={ToggleStyle.Circle}></View>
         </TouchableOpacity>
     )
--- src/components/Reuse/Toggle/style/style.tsx
+++ src/components/Reuse/Toggle/style/style.tsx
@@ -3,7 +3,15 @@ import { Screen_Size } from "../../../DesigneTokens/metrics";
 import { Primmary_Colors, Secondary_Colors } from "../../../DesigneTokens/pallets";
 
 const ToggleStyle = StyleSheet.create({
-    toggle: {
+    onToggle: {
+        display: 'flex',
+        alignItems: 'flex-start',
+        width: 150 * (Screen_Size.width / 1080),
+        height: 70 * (Screen_Size.width / 1080),
+        backgroundColor: Secondary_Colors.White70,
+        borderRadius: 100 * (Screen_Size.width / 1080),
+    },
+    offToggle: {
         display: 'flex',
         alignItems: 'flex-start',
         width: 150 * (Screen_Size.width / 1080),
