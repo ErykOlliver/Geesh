f0f9a8e2791967cdaab4ca074fc6cadf21cac765
Eryk Olliver <erykolliver@gmail.com>
2025-08-24 14:43:50 -0300

    config: metricas corrigidas

--- src/components/DesigneTokens/metrics.js
+++ src/components/DesigneTokens/metrics.js
@@ -2,22 +2,34 @@ import { Dimensions } from "react-native";
 
 const { width, height } = Dimensions.get('screen');
 
-export const Text_Sizes = {
-    h1: 32,
-    h2: 20,
-    body: 16,
-    small: 14,
-    p: 12
+const Text_Sizes = {
+    h1: 100 * (width / 1080),
+    h2: 48 * (width / 1080),
+    h3: 40 * (width / 1080),
+    h4: 36 * (width / 1080),
+    h5: 32 * (width / 1080),
+    h6: 24 * (width / 1080),
+    body: 16 * (width / 1080),
+    small: 14 * (width / 1080),
+    p: 12 * (width / 1080)
 }
 
-export const Icon_Sizes = {
-    IconXL: 48,
-    IconL: 32,
-    IconM: 24,
-    IconS: 20
-}
-
-export const Screen_Size = {
+const Screen_Size = {
     width: width,
     height: height
 }
+
+
+const Icon_Size = {
+    Icon5xl: 110 * (width / 1080),
+    Icon4xl: 94 * (width / 1080),
+    Icon3xl: 76 * (width / 1080),
+    Icon2xl: 68 * (width / 1080),
+    Icon1xl: 54 * (width / 1080),
+    IconXL: 48 * (width / 1080),
+    IconL: 32 * (width / 1080),
+    IconM: 24 * (width / 1080),
+    IconS: 20 * (width / 1080)
+}
+
+export { Screen_Size, Icon_Size, Text_Sizes }
